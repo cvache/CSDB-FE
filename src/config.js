c6867b04c67b4bd43d58a144c9b09e830ac837e1
cvache <vachecarson@seattleu.edu>
2020-07-28 15:20:48 -0700

    Add netlify build script

--- src/config.js
+++ src/config.js
@@ -1,17 +1,43 @@
-export default {
-    MAX_ATTATCHMENT_SIZE: 5000000,
+const dev = {
     s3: {
-        REGION: "us-west-2",
-        BUCKET: "cv-notes-app-uploads"
+        REGION: 'us-west-2',
+        BUCKET: 'notes-app-2-api-dev-attachmentsbucket-1g3vc0lyemlpu'
     },
     apiGateway: {
         REGION: 'us-west-2',
-        URL: 'https://ukil9y3kzf.execute-api.us-west-2.amazonaws.com/prod'
+        URL: 'https://nuweyjuh71.execute-api.us-west-2.amazonaws.com/dev'
     },
     cognito: {
-        REGION: "us-west-2",
-        USER_POOL_ID: "us-west-2_aZXcv3NTU",
-        APP_CLIENT_ID: "4p8ehpuob06ddsqe1i8hvmmveh",
-        IDENTITY_POOL_ID: "us-west-2:d94f9ab9-f4a5-4461-ad86-2dfc0b8dfff9"
+        REGION: 'us-west-2',
+        USER_POOL_ID: 'us-west-2_NIy0K9EKo',
+        APP_CLIENT_ID: '6t45hmcb5mv7pdpgpgnrob8k9i',
+        IDENTITY_POOL_ID: 'us-west-2:4d27308c-ea44-4df0-b13e-2219e2c27fb2'
     }
+};
+
+const prod = {
+    s3: {
+        REGION: 'us-west-2',
+        BUCKET: 'notes-app-2-api-prod-attachmentsbucket-gdmu222gnqeg'
+    },
+    apiGateway: {
+        REGION: 'us-west-2',
+        URL: 'https://lbu9yishii.execute-api.us-west-2.amazonaws.com/prod'
+    },
+    cognito: {
+        REGION: 'us-west-2',
+        USER_POOL_ID: 'us-west-2_KRQlBFfk6',
+        APP_CLIENT_ID: '2nm4944db8ps6jugovadsm9faj',
+        IDENTITY_POOL_ID: 'us-west-2:0a84d65d-20b2-4936-9d1c-8a7b1018ad76'
+    }
+};
+
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+
+export default {
+    MAX_ATTATCHMENT_SIZE: 5000000,
+    ...config
 };
