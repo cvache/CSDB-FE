7497aad55a621375d6df2fee5b4aea128d62d8f5
cvache <vachecarson@seattleu.edu>
2020-07-28 13:28:38 -0700

    Fixed newNote failing with error 500

--- src/Routes.js
+++ src/Routes.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import Home from './containers/Home';
-import NotFound from './containers/NotFound';
-import Login from './containers/Login';
-import Signup from './containers/Signup';
-import NewNote from './containers/NewNote';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./containers/Home";
+import Login from "./containers/Login";
 import Notes from "./containers/Notes";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
-    return(
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-
-            <Route exact path="/login">
-                <Login />
-            </Route>
-
-            <Route exact path="/signup">
-                <Signup />
-            </Route>
-
-            <Route exact path="/notes/:id">
-                <Notes />
-            </Route>
-
-            <Route exact path="/notes/new">
-                <NewNote/>
-            </Route>
-
-            {/* Catch All */}
-            <Route>
-                <NotFound/>
-            </Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/notes/new">
+        <NewNote />
+      </Route>
+      <Route exact path="/notes/:id">
+        <Notes />
+      </Route>
+      {/* Finally, catch all unmatched routes */}
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
 }
