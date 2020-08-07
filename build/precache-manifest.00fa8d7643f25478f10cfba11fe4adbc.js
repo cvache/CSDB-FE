0ae3eb092202dec4eecd9d5b1830e6adb3b22d58
cvache <vachecarson@seattleu.edu>
2020-08-06 18:01:02 -0700

    Prep for build

---
+++ build/precache-manifest.00fa8d7643f25478f10cfba11fe4adbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81ffe8a5e9aa40b476026d300b1f450",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5cf9371b992b3da0369",
+    "url": "/static/css/main.e5289168.chunk.css"
+  },
+  {
+    "revision": "fe68623e442052ed3e6c",
+    "url": "/static/js/2.c740ee50.chunk.js"
+  },
+  {
+    "revision": "2c69aaf6155d411bfd5915efbd3f1059",
+    "url": "/static/js/2.c740ee50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5cf9371b992b3da0369",
+    "url": "/static/js/main.ab15451d.chunk.js"
+  },
+  {
+    "revision": "8e83ee1332e0b879fbf0",
+    "url": "/static/js/runtime-main.6d977fc8.js"
+  }
+]);
