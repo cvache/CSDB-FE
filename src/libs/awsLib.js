fbf24ff89be0c418460478ae9c4b3b30f3e8c51f
cvache <vachecarson@seattleu.edu>
2020-08-18 17:33:44 -0700

    working

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -3,8 +3,7 @@ import { Storage } from "aws-amplify";
 export async function s3Upload(file) {
   const filename = `${Date.now()}-${file.name}`;
 
-  const stored = await Storage.vault.put(filename, file, {
-    level: 'protected',
+  const stored = await Storage.put(filename, file, {
     contentType: file.type,
   });
 
